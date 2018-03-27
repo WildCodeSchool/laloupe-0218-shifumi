bcd124d24d61549cf932f24e61c275fc9b2a3c7c
Martin DONADIEU <martindonadieu@gmail.com>
2018-03-28 01:47:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ owner="$(cut -d'/' -f1 <<< ${TRAVIS_REPO_SLUG})"
 repo="$(cut -d'/' -f2 <<< ${TRAVIS_REPO_SLUG})"
 
 if [ -z "${TRAVIS_PULL_REQUEST}" -o "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  npm run build --prod --base-href "https://${owner}.github.io/${repo}/" angular-cli-ghpages
+  npm run build --prod -- --base-href "https://${owner}.github.io/${repo}/" -dop false
+  angular-cli-ghpages
 else
-  npm run build --prod --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" --dir "./pr/${TRAVIS_PULL_REQUEST}" angular-cli-ghpages
-  npm run lh --score=80 https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/
+  npm run build --prod -- --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" -op "./dist/pr/${TRAVIS_PULL_REQUEST}" -dop false
+  angular-cli-ghpages
+  npm run lh --score=70 https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/
 fi

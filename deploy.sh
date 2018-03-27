e66e9c0f29bc7001bd04ddc0be6e060ba8022c1e
Martin DONADIEU <martindonadieu@gmail.com>
2018-03-27 22:34:51 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+owner="$(cut -d'/' -f1 <<< ${TRAVIS_REPO_SLUG})"
+repo="$(cut -d'/' -f2 <<< ${TRAVIS_REPO_SLUG})"
+
+if [ -z "${TRAVIS_PULL_REQUEST}" -o "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  npm run build --prod --base-href "https://${owner}.github.io/${repo}/" angular-cli-ghpages
+else
+  npm run build --prod --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" --dir "./pr/${TRAVIS_PULL_REQUEST}" angular-cli-ghpages
+  npm run lh --score=70 https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/
+fi

beda70eccd588d4ca6da560b524eeab1eff78f46
Martins Alexandre <xeos28@gmail.com>
2018-03-29 14:15:07 +0200

    error key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase, ' shifumi '),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features

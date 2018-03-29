855fde23f9a0a3827f67c9a7ba710791535c3153
Martin DONADIEU <martindonadieu@gmail.com>
2018-03-29 16:08:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 owner="$(cut -d'/' -f1 <<< ${TRAVIS_REPO_SLUG})"
 repo="$(cut -d'/' -f2 <<< ${TRAVIS_REPO_SLUG})"
 
-if [ -z "${TRAVIS_PULL_REQUEST}" -o "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  npm run build --prod -- --base-href "https://${owner}.github.io/${repo}/" -dop false
-  angular-cli-ghpages
-else
-  npm run build --prod -- --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" -op "./dist/pr/${TRAVIS_PULL_REQUEST}" -dop false
-  angular-cli-ghpages
+if [ -n "${TRAVIS_PULL_REQUEST}" -a "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  npm run build -- --base-href "https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/" -op "./dist/pr/${TRAVIS_PULL_REQUEST}" -dop false
+  angular-cli-ghpages --repo=https://GH_TOKEN@github.com/${TRAVIS_REPO_SLUG}.git --name="Travis bro" --email=travis@wildcodeschool.fr
   npm run lh --score=70 https://${owner}.github.io/${repo}/pr/${TRAVIS_PULL_REQUEST}/
+elif [ "${TRAVIS_BRANCH}" = "master" ]; then
+  npm run build -- --base-href "https://${owner}.github.io/${repo}/" -dop false
+  angular-cli-ghpages --repo=https://GH_TOKEN@github.com/${TRAVIS_REPO_SLUG}.git --name="Travis bro" --email=travis@wildcodeschool.fr
 fi

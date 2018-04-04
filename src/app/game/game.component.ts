0ea844ec96e4af79e69d1ba8ddc7ea5020a93266
Martins Alexandre <xeos28@gmail.com>
2018-04-04 16:24:41 +0200

    waiting player and enter the game is OK

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,6 +4,8 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Room } from '../models/room';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -13,11 +15,35 @@ import * as firebase from 'firebase/app';
 })
 export class GameComponent implements OnInit {
 
+  message = 'Waiting for opponent';
+  roomId: string;
+  username: string;
+  room: Room;
+  myPlayerId: number;
+
   choice: Observable<any[]>;
-  constructor(public auth: AuthService, db: AngularFirestore) {
+  rooms: Observable<any[]>;
+  constructor(private route: ActivatedRoute, public auth: AuthService, private db: AngularFirestore) {
     this.choice = db.collection('choice').valueChanges();
+    this.rooms = db.collection('rooms').valueChanges();
   }
   ngOnInit() {
+    this.roomId = this.route.snapshot.paramMap.get('id');
+    this.username = this.route.snapshot.paramMap.get('username');
+
+    this.db
+      .doc<Room>('rooms/' + this.roomId)
+      .valueChanges()
+      .subscribe((room) => {
+        this.room = room;
+        this.myPlayerId = room.players[0].name === this.username ? 0 : 1;
+        if (room.players.length === 2) {
+          this.message = 'Starting game';
+          if (room.players[0].name === this.username) {
+
+          }
+        }
+      });
   }
 
 }
--- src/app/match-making/match-making.component.ts
+++ src/app/match-making/match-making.component.ts
@@ -47,4 +47,3 @@ export class MatchMakingComponent implements OnInit {
     });
   }
 }
-
